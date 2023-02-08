b69dc79f4a5e38ee1904a9bcf4c55dc8914d0596
nurikgentle <nurikgentle@gmail.com>
2023-02-09 00:09:23 +0600

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/Home.scss";
+import toCatalog from "../assets/toCatalog.svg";
+import redPen from "../assets/redPen.svg";
+import whitePen from "../assets/whitePen.svg";
+import call from "../assets/calling.svg";
+import linear from "../assets/linear.svg";
 
 const Home = () => {
   return (
     <div className="wrapper">
-
+      <div className="header">
+        <div className="left">
+          <h1>Канцелярия, товары для полиграфии</h1>
+          <p>
+            Основные направления – бумага и расходные материалы для полиграфии,
+            поставки элитного кофе и чая, немецкого премиум пива.
+          </p>
+          <img src={toCatalog} />
+        </div>
+        <div className="right">
+          <img src={redPen} />
+          <img src={whitePen} />
+          <img src={linear} />
+          <img src={call} />
+        </div>
+      </div>
     </div>
   );
 };
